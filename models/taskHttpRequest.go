7e74cc7d32b97c5e797cb658509254d96658c070
ImRi8 <singh.rishabh701@gmail.com>
2023-12-13 23:50:38 +0530

    Added HTTP models

--- models/taskHttpRequest.go
+++ models/taskHttpRequest.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+type Request struct {
+	ID          int64     `json:"id"`
+	IsShadowed  bool      `json:"isShadowed"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Priority    int64     `json:"priority"`
+	DueDate     time.Time `json:"dueDate"`
+}
--- models/taskHttpResponse.go
+++ models/taskHttpResponse.go
@@ -1,1 +1,17 @@
 package models
+
+import "time"
+
+type Response struct {
+	Status      string    `json:"status,omitempty"`
+	Message     string    `json:"message,omitempty"`
+	HttpStatus  int       `json:"httpStatus,omitempty"`
+	ID          int64     `json:"id"`
+	IsShadowed  bool      `json:"isShadowed"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Priority    int64     `json:"priority"`
+	DueDate     time.Time `json:"dueDate"`
+}
